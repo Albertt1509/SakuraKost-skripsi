2e0abea5c9b1bd31bfb35f6b6ef6cdcf57d44e51
Albertus Prido Nathanael <94915933+Albertt1509@users.noreply.github.com>
2024-02-06 20:31:19 +0700

    update

--- BE/index.js
+++ BE/index.js
@@ -8,12 +8,7 @@ const app = express();
 const path = require("path");
 
 app.use(express.json());
-app.use(
-  cors({
-    credentials: true,
-    origin: [],
-  })
-);
+app.use(cors());
 mongoose
   .connect(process.env.MONGO, {
     useNewUrlParser: true,
