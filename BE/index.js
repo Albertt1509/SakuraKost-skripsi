0cfa84c68dc62d1767145f2a71817003422de605
Albertus Prido Nathanael <94915933+Albertt1509@users.noreply.github.com>
2024-02-07 10:05:21 +0700

    update

--- BE/index.js
+++ BE/index.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const routes = require('./src/routes/route.js')
+const routes = require('./src/routes/route.js');
 require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const app = express();
 const path = require("path");
 
 app.use(express.json());
-app.use(cors());
+const corsConfig = {
+  origin: "*",
+  credentials: true,
+  methods: ["GET", "UPDATE", "DELETE", "PUT", "USE"]
+};
+app.use(cors(corsConfig));
+
 mongoose
   .connect(process.env.MONGO, {
     useNewUrlParser: true,
@@ -17,16 +23,18 @@ mongoose
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Error connecting to MongoDB:", err));
 app.use(cookieParser());
-//user connected
+
+// Serve static files
 app.use("/images", express.static(path.join(__dirname, "images")));
 app.use("/foto", express.static(path.join(__dirname, "foto")));
 
 app.use("/", routes);
 
-app.get("/", (req, res) => {
+// Handle undefined routes
+app.get("/", (req, res, next) => {
   res.json("🦄🌈💰💸💳");
 });
 
 app.listen(4000, () => {
-  console.log("Connect on port 4000");
+  console.log("Connected on port 4000");
 });
