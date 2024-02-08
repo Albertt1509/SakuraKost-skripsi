b16a6cc0a6966baaeb8da41c54bbabe862a5947c
Albertus Prido <94915933+Albertt1509@users.noreply.github.com>
2024-02-08 10:15:34 +0700

    Update index.js

--- BE/index.js
+++ BE/index.js
@@ -9,9 +9,10 @@ const path = require("path");
 
 app.use(express.json());
 const corsConfig = {
-  origin: "https://vast-cyan-ray-tux.cyclic.app",
+  origin: ["https://vast-cyan-ray-tux.cyclic.app","http://localhost:5173","http://127.0.0.1:5173/"]
   credentials: true,
-  methods: ["GET", "UPDATE", "DELETE", "PUT", "POST"] 
+  methods: ["GET", "UPDATE", "DELETE", "PUT", "POST"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 };
 
 app.use(cors(corsConfig));
